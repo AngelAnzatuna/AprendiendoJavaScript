b2d4ba2e92917d51e225c555728ecedd1e52e998
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-09 23:06:58 +0100

    Agregar formato de fecha y calendario en español utilizando Moment.js

--- 21-Fechas/js/02-app.js
+++ 21-Fechas/js/02-app.js
@@ -1,5 +1,6 @@
 const diaHoy = new Date();
 moment.locale('es');
 
-console.log(moment().format('MMMM Do'))
-console.log(moment().format('LLLL'))
+console.log(moment().format('MMMM Do'));
+console.log(moment().format('LLLL'));
+console.log(moment().add(3, 'days').calendar());
