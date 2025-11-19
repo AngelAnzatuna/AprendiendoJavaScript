992e3fa890ee4ddde078ee7e2c9c17693199bb80
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-19 16:50:29 +0100

    Agregar implementación de clases Cliente y Cliente2 en el archivo 01-app.js

---
+++ 24-POO/js/01-app.js
@@ -0,0 +1,23 @@
+// Class declaretion
+class Cliente {
+    constructor(nombre, saldo) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+}
+
+// Intanciar
+const angel = new Cliente('Angel', 1000);
+console.log(angel);
+
+// Class expression
+const Cliente2 = class {
+    constructor(nombre, saldo) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+}
+
+// Instanciar
+const angel2 = new Cliente2('Angel', 1000);
+console.log(angel2);
