df65f9cd80dd8333336e605e57fd0819a4eac4ae
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-12-01 18:50:37 +0100

    Introducción al uso del set, métodos más comunes.

---
+++ 27-SetsMapsSymbols/js/01-app.js
@@ -0,0 +1,24 @@
+const carrito = new Set();
+
+carrito.add('Camisa');
+carrito.add('Disco #1');
+carrito.add('Disco #2');
+carrito.add('Disco #3');
+
+// carrito.delete('Disco #2');
+
+// console.log(carrito.has('Camisa'));
+
+console.log(carrito.size);
+
+// carrito.clear();
+carrito.forEach( producto => {
+    console.log(producto);
+})
+
+console.log(carrito);
+
+// Del siguiente arreglo, eliminar los duplicados 
+const numeros = [10,20,30,40,10,20];
+const noDuplicados = new Set(numeros);
+console.log(noDuplicados);
