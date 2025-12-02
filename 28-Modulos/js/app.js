1c5dcbc1cf9c61b0fb5da7416308987045e5235f
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-12-02 21:24:54 +0100

    Heredando clase Cliente en Empresa en documentos empresa.js/ Importando empresa.js en app.js

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,5 @@
 import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente} from "./cliente.js";
+import { Empresa } from "./empresa.js";
 
 console.log(nombreCliente);
 console.log(ahorro);
@@ -12,3 +13,8 @@ const cliente = new Cliente(nombreCliente, ahorro);
 console.log(cliente);
 
 console.log(cliente.mostrarInformacion());
+
+// Importar empresa
+
+const empresa = new Empresa('Dabxis', 1000, 'Aprendizaje');
+console.log(empresa.mostrarInformacion());
