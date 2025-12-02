fd9a12bebb9b1652feb2b4a7182f56ba6df982ed
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-12-02 21:03:13 +0100

    Exportando e importando funciones entre archivos js

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,3 +1,8 @@
-import { nombreCliente, ahorro } from "./cliente.js";
-console.log(nombreCliente)
-console.log(ahorro)
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo } from "./cliente.js";
+
+console.log(nombreCliente);
+console.log(ahorro);
+
+console.log(mostrarInformacion(nombreCliente, ahorro));
+
+tieneSaldo(ahorro);
