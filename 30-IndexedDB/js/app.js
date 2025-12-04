8a4601a8b4591028798abd9626558a800c78f0eb
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-12-04 19:18:05 +0100

    Creando una BD con indexedDB

---
+++ 30-IndexedDB/js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    crmDB()
+})
+
+function crmDB() {
+    // Crear base de datos version 1.0
+    let crmDB = window.indexedDB.open('crm', 1);
+
+    // Si hay un error
+    crmDB.onerror = function() {
+        console.log('Hubo un error a la hora de crear la BD');
+    }
+
+    // Si se creo bien
+    crmDB.onsuccess = function() {
+        console.log('Base de datos creada');
+    }
+    // Configuración de la base de datos
+    crmDB.onupgradeneeded = function() {
+        console.log('Este método se ejecuta una sola vez')
+    }
+}
