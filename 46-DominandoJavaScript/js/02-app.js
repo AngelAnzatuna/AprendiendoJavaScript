1ece826e6e4c64055613f953f4baf89e34fd407f
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-13 10:48:03 +0100

    feat: demostrar hoisting en funciones declaradas y expresadas

---
+++ 46-DominandoJavaScript/js/02-app.js
@@ -0,0 +1,10 @@
+obtenerCliente('Angel');
+
+function obtenerCliente(nombre) {
+    console.log(`El nombre del cliente en es ${nombre}`);
+}
+
+obtenerCliente2('Alexis');
+const obtenerCliente2 = function(nombre) {
+    console.log(`El nombre del cliente en es ${nombre}`);
+}
