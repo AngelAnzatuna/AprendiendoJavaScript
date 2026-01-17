454774289727ef0c6347347f795de30196fadc73
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-17 22:56:02 +0100

    feat: agregar ejemplo de class pattern

---
+++ 48-DesignPatterns/js/01-app.js
@@ -0,0 +1,12 @@
+// Class Pattern
+
+class Persona {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const persona = new Persona('Angel', 'angel@correo.com');
+
+console.log(persona);
