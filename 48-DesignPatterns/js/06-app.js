4054d3f12f55d4b97da89cad41fe1a7ce4d6baec
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-18 17:50:04 +0100

    feat: agregar ejemplo de mixin pattern

---
+++ 48-DesignPatterns/js/06-app.js
@@ -0,0 +1,37 @@
+class Persona {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+class Cliente {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const funcionesPersona = {
+    mostrarInformacion() {
+        console.log(`Nombre Persona: ${this.nombre} Email: ${this.email}`)
+    },
+    mostrarNombre() {
+        console.log(`Mi nombre es ${this.nombre}`)
+    }
+}
+
+// Añadir funcionesPersona a la clase de Persona
+Object.assign(Persona.prototype, funcionesPersona);
+Object.assign(Cliente.prototype, funcionesPersona);
+
+const cliente = new Persona('Angel', 'angel@correo.com');
+
+console.log(cliente);
+cliente.mostrarInformacion();
+cliente.mostrarNombre();
+
+const cliente2 = new Cliente('cliente', 'cliente@correo.com');
+console.log(cliente2);
+cliente2.mostrarInformacion();
+cliente2.mostrarNombre();
