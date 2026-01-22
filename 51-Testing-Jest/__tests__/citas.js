3c817a4a6b2c2e325aa0c294c10372d2a9267711
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-22 08:35:52 +0100

    feat: agregar ejemplo de testing para los métodos actualizar y eliminar de la clase Citas

--- 51-Testing-Jest/__tests__/citas.js
+++ 51-Testing-Jest/__tests__/citas.js
@@ -3,23 +3,44 @@ import Citas from "../js/classes/Citas";
 describe('Probar la clase de Citas', () => {
 
     const citas = new Citas();
+    const id = Date.now();
 
     test('Agregar una nueva cita', () => {
         const citaObj = {
+            id,
             mascota: 'Hulk',
             propietario: 'Angel',
             telefono: '1234567890',
             fecha: '10-12-2026',
             hora: '10:30',
             sintomas: 'Solo duerme',
-
         };
-
-        citaObj.id = Date.now();
 
         citas.agregarCita(citaObj);
 
         // Prueba
         expect(citas).toMatchSnapshot()
     });
+
+    test('Actualizar cita', () => {
+        const citaActualizada = {
+            id,
+            mascota: 'Nuevo Nombre',
+            propietario: 'Angel',
+            telefono: '1234567890',
+            fecha: '10-12-2026',
+            hora: '10:30',
+            sintomas: 'Solo duerme',
+        };
+
+        citas.editarCita(citaActualizada);
+
+        expect(citas).toMatchSnapshot();
+    });
+
+    test('Eliminar cita', () => {
+        citas.eliminarCita(id);
+
+        expect(citas).toMatchSnapshot();
+    })
 });
